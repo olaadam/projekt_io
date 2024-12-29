ced7617ea64fb74850705dd3515167dcc76efa99
olaadam <89353626+olaadam@users.noreply.github.com>
2024-12-29 14:31:49 +0100

    Delete apka/app directory

--- apka/app/backend/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-
-def create_app() -> Flask:
-    app = Flask(__name__, static_folder="../frontend/static", template_folder="../frontend/templates")
-    from .routes import main
-    app.register_blueprint(main)
-    return app
